a67890c1db7af5b5a10b202ba855f5c804e05c0c
Jakub <45879657+zeedivx@users.noreply.github.com>
2025-12-19 10:18:19 +0100

    style: format __all__ exports in __init__.py for improved readability

--- src/authly_sdk/__init__.py
+++ src/authly_sdk/__init__.py
@@ -2,4 +2,10 @@ from .client import AuthlyClient
 from .schemas import Claims
 from .exceptions import AuthlyError, TokenExpiredError, TokenInvalidError
 
-__all__ = ["AuthlyClient", "Claims", "AuthlyError", "TokenExpiredError", "TokenInvalidError"]
+__all__ = [
+    "AuthlyClient",
+    "Claims",
+    "AuthlyError",
+    "TokenExpiredError",
+    "TokenInvalidError",
+]
